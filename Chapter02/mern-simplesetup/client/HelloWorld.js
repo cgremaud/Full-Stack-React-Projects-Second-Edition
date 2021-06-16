3af0e253517bfb93eae11436748b46b2f5db94e8
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-16 15:00:09 -0500

    installed dependencies in ch. 2 successfully

--- Chapter02/mern-simplesetup/client/HelloWorld.js
+++ Chapter02/mern-simplesetup/client/HelloWorld.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
+
+
 const HelloWorld = () => {
     return (
         <div>
-          <h1>Hello World!</h1>
+          <h1>hello</h1>
         </div>
       )
 }
